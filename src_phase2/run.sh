a9dfd3e8cb1f65809fd33b6b2c3a2bd62c498a42
Siddharth Nayak <siddharthnayak98@gmail.com>
2018-12-04 15:42:11 +0530

    Kubernete Access Files added

---
+++ src_phase2/run.sh
@@ -0,0 +1,15 @@
+# Note: all paths referenced here are relative to the Docker container.
+#
+# Add the Nvidia drivers to the path
+export PATH="/usr/local/nvidia/bin:$PATH"
+export LD_LIBRARY_PATH="/usr/local/nvidia/lib:$LD_LIBRARY_PATH"
+# Tools config for CUDA, Anaconda installed in the common /tools directory
+source /tools/config.sh
+# Activate your environment
+source activate py35
+# Change to the directory in which your code is present
+cd /storage/home/sidnayak/Reinforcement-Learning-for-Object-Detection/src_phase2/run.sh
+# Run the code. The -u option is used here to use unbuffered writes
+# so that output is piped to the file as and when it is produced.
+# Here, the code is the MNIST Tensorflow example.
+python -u Episodic_autoreinforce_2_actions.py --episodes=1000 --tmax=10 --load_model=1 --threshold=0.94 &> out
