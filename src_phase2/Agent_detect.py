3bd53b569f6d81208c0de11c12fa4396585624c0
Siddharth Nayak <siddharthnayak98@gmail.com>
2018-12-17 14:00:20 +0530

    Test Labels

--- src_phase2/Agent_detect.py
+++ src_phase2/Agent_detect.py
@@ -157,8 +157,8 @@ def main():
         image_list = shuffle_arr(image_list) #shuffle_arr from utils.py shuffles the array randomly
         reward_arr=[]
         # for episodes in range(args.episodes):
-        # for episodes in tqdm(range(num_images)):
-        for episodes in tqdm(range(3)):
+        for episodes in tqdm(range(num_images)):
+        # for episodes in tqdm(range(3)):
             img_name = image_list[episodes]
             # index_img = np.random.uniform(low = 0, high = num_images, size = (1,)).astype(int) # random number between 0 and num_images eg: 435
             # img_name = os.listdir(image_filepath)[index_img[0]] # eg: 000005.jpg
