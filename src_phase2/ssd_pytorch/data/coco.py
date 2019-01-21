a39d8b8fbe1930a6f19094f53a31245356ccf47b
Siddharth Nayak <siddharthnayak98@gmail.com>
2019-01-21 14:19:23 +0530

    Print statements

--- src_phase2/ssd_pytorch/data/coco.py
+++ src_phase2/ssd_pytorch/data/coco.py
@@ -9,8 +9,6 @@ import cv2
 import numpy as np
 
 COCO_ROOT = osp.join(HOME, 'data/coco/')
-print(COCO_ROOT)
-print()
 IMAGES = 'images'
 ANNOTATIONS = 'annotations'
 COCO_API = 'PythonAPI'
@@ -46,9 +44,6 @@ class COCOAnnotationTransform(object):
     Initilized with a dictionary lookup of classnames to indexes
     """
     def __init__(self):
-        print()
-        print(osp.join(COCO_ROOT, 'coco_labels.txt'))
-        print()
         self.label_map = get_label_map(osp.join(COCO_ROOT, 'coco_labels.txt'))
 
     def __call__(self, target, width, height):
