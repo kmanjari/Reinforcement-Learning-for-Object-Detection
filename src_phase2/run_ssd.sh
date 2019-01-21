d44e55b23d021582310ec82ba6adaf9ba8e20fe8
nsidn98 <siddharthnayak98@gmail.com>
2019-01-21 15:10:00 +0530

    Print

--- src_phase2/run_ssd.sh
+++ src_phase2/run_ssd.sh
@@ -11,5 +11,5 @@ source activate py35
 cd /storage/home/sidnayak/Reinforcement-Learning-for-Object-Detection/src_phase2
 # Run the code. The -u option is used here to use unbuffered writes
 # so that output is piped to the file as and when it is produced.
-# Here, the code is the MNIST Tensorflow example.
+
 python -u ssd_baseline.py &> ssd_out
