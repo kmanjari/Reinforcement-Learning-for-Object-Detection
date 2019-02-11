942def65e08fa16e77f62900f5a17c57fad77d9d
nsidn98 <siddharthnayak98@gmail.com>
2019-02-12 00:25:51 +0530

    Kubernete files changed

--- src_phase2/run_ssd.sh
+++ src_phase2/run_ssd.sh
@@ -13,5 +13,5 @@ cd /storage/home/sidnayak/Reinforcement-Learning-for-Object-Detection/src_phase2
 # Run the code. The -u option is used here to use unbuffered writes
 # so that output is piped to the file as and when it is produced.
 
-python -u ssdAgentDetect.py --load_model=1 --epoch=50 --lr=1e-5 &> outputs/ssd_training
+python -u ssdAgentDetect.py --load_model=1 --epoch=20 --lr=1e-6 &> outputs/ssd_training_50
 #python -u ssd_random.py --epoch=10 &> outputs/out_random
